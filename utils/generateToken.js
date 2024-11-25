23a2978602ef9a8c88f6ea92f858503aff701dbd
DaniAm94 <dani.amico94@gmail.com>
2024-11-25 12:39:05 +0100

    fixed JTW to JWT

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,4 +2,4 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 module.exports = (payload, expiresIn = "30d") =>
-  jwt.sign(payload, process.env.JTW_SECRET_KEY, { expiresIn });
+  jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn });
