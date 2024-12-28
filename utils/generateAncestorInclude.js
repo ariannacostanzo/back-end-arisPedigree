60df156dc0b8301e50d45d3258e2c45f684716dd
ariannacostanzo <costanzo.arianna@outlook.it>
2024-12-28 17:10:21 +0100

    adding children as parent for dog's parents

--- utils/generateAncestorInclude.js
+++ utils/generateAncestorInclude.js
@@ -4,15 +4,17 @@
  * @returns {Object} Un oggetto che contiene ricorsivamente un campo include
  */
 const generateAncestorInclude = (depth) => {
-    if (depth === 0) return {};
-    return {
-        include: {
-            breed: true,
-            country: true,
-            sire: generateAncestorInclude(depth - 1),
-            dam: generateAncestorInclude(depth - 1)
-        }
-    };
+  if (depth === 0) return {};
+  return {
+    include: {
+      breed: true,
+      country: true,
+      sire: generateAncestorInclude(depth - 1),
+      dam: generateAncestorInclude(depth - 1),
+      childrenAsDam: true,
+      childrenAsSire: true,
+    },
+  };
 };
 
 module.exports = generateAncestorInclude;
